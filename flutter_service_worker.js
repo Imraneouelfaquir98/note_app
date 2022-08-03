6e74c820a39d241e2f0c4b7b331051d71b7861bb
Imrane OU EL FAQUIR <74380307+Imraneouelfaquir98@users.noreply.github.com>
2022-08-03 17:12:15 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "6f00af5b476d706edb49277ce1f61217",
-"/": "6f00af5b476d706edb49277ce1f61217",
+  "index.html": "4926df6fac96d79de087ef751c496858",
+"/": "4926df6fac96d79de087ef751c496858",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -12,7 +12,7 @@ const RESOURCES = {
 "manifest.json": "b3ca37f878e33f420cb2dfdcfb94d15b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "15f4c7e13fcc17a64a2303783db50dda",
-"main.dart.js": "ac33cbfd1ab5931f67ca32b5f1fdad6a",
+"main.dart.js": "35e4f8fc08ab130cfca5d9bceab722c6",
 "assets/AssetManifest.json": "b4cdf0d8881009f778304e85008933ca",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
